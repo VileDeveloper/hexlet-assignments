bbc224a2ab0c7a5b5954f60b367a1f821dcfcea7
@hexlet/cli <support@hexlet.io>
2024-01-15 21:52:12 +0600

    submit rails-basics-ru/rack

---
+++ rails-basics-ru/rack/lib/app/execution_timer.rb
@@ -0,0 +1,16 @@
+class ExecutionTimer
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    start_time = Time.now
+    status, headers, response = @app.call(env)
+    end_time = Time.now
+    execution_time = end_time - start_time
+
+    headers['X-Execution-Time'] = execution_time.to_s
+
+    [status, headers, response]
+  end
+end
---
+++ rails-basics-ru/rack/lib/app/signature.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'digest'
+
+class Signature
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    status, headers, body = @app.call(env)
+    sha256 = Digest::SHA2.hexdigest(body.join)
+    body << sha256
+
+    [status, headers, body]
+  end
+end
