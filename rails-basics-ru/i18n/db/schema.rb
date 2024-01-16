8d916482038e5ca4f27a783eaf277e6ad770b0ef
Vladimir Litov <vladimirlitov131127@gmail.com>
2024-01-16 21:53:26 +0600

    fix migration

--- rails-basics-ru/i18n/db/schema.rb
+++ rails-basics-ru/i18n/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_09_22_101929) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_22_101929) do
   create_table "post_comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id", null: false
