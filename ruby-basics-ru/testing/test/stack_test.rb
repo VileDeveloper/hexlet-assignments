5676741261301943eb81fd8671b470ee1dffad90
@hexlet/cli <support@hexlet.io>
2024-05-14 16:58:09 +0600

    submit ruby-basics-ru/testing

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -5,40 +5,7 @@ require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
   # BEGIN
-  def test_push!
-    stack = Stack.new %w[one two three]
-    stack.push! 'uno'
-    stack.push! 'dundo'
-    stack.push! 'freedo'
-
-    assert { stack.to_a == %w[one two three uno dundo freedo] }
-    assert { stack.size == 6 }
-  end
-
-  def test_pop
-    stack = Stack.new %w[one two three]
-    stack.pop!
-    stack.pop!
-
-    assert { stack.to_a == %w[one] }
-    assert { stack.size == 1 }
-  end
-
-  def test_clear!
-    stack = Stack.new %w[one two three]
-    assert { !stack.empty? }
-
-    stack.clear!
-    assert { stack.empty? }
-  end
-
-  def test_empty_stack
-    stack = Stack.new
-
-    assert { stack.empty? == true }
-    assert { stack.to_a == [] }
-    assert { stack.pop!.nil? }
-  end
+  
   # END
 end
 
